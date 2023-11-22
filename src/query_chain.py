40222a01370b6c924dd20c0ab42b4367eb66c150
Rishav Roy <rr4577@log-2.hpc.nyu.edu>
2023-11-22 18:02:16 -0500

    merge Llama-FAISS and Mistral-Chroma workflow

--- src/query_chain.py
+++ src/query_chain.py
@@ -1,17 +1,24 @@
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma, FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 
-embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
 print("loading vector_db ....")
-db = Chroma(persist_directory="./chroma_db", embedding_function=embedding)
+
+embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+db = "faiss"
+
+if db == "faiss":
+    persist_directory = 'faiss_db/'
+    db = FAISS.load_local(persist_directory, embeddings=embedding)
+elif db == "chroma":
+    db = Chroma(persist_directory="./chroma_db", embedding_function=embedding)
+else:
+    raise ValueError(f"Cannot recognize database - {db}")
 
 print("quering question ....")
 question = "What is HELIS?"
 result = db.similarity_search(question, k=3)
 
-for i in range(3):
+for i in range(len(result)):
     print(f"document {i}")
     print(result[i].page_content)
-    print("")
+    print(f"\n")
